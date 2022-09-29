92e2032164d8967465477e7586ce4bda0d91acf0
GJXS1980 <gjxslwz@gmail.com>
2022-09-29 15:10:01 +0800

    ai book

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#sudo
+sudo dpkg -i ros-melodic-arvlidar-ros_amd64.deb
+
+cd castlex_follow/skeleton_track/nite2/lib/NiTE2/Data/
+unzip lbsdata.zip
+sudo chmod 777 lbsdata.lbd 
+
+
+
+
--- install_rplidar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#sudo
-sudo dpkg -i ros-melodic-arvlidar-ros_amd64.deb
-
